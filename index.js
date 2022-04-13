43208712342a7be3c781ffdf4524bccadf7c49a9
Jake Hankins <jakehankins@comcast.net>
2022-04-13 19:49:11 -0400

    commit tree c68dc3f8426ecde066e33562a6bb029f6c7214fc

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+const employee = {
+    name: "Jake",
+    streetAddress: "100 Main Street",
+}
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const newEmployee = {...employee};
+    newEmployee[key]= value;
+    return newEmployee;
+
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value
+    return employee
+}
+function deleteFromEmployeeByKey(employee,key){
+    const newEmployees = {...employee}
+    delete newEmployees.name
+    return newEmployees
+}
+function destructivelyDeleteFromEmployeeByKey(employee,key){
+    delete employee.name
+    return employee
+}
